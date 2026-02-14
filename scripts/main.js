7efe0e9ca1b8d3a49fe7643287aae51e24399a2e
LukaPatarcic <luka.patarcic@gorgias.com>
2026-02-14 19:53:55 +0100

    feat: starter template

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// Compacc - Main JavaScript
+// This file can be expanded with additional functionality as needed
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Compacc website loaded');
+});
